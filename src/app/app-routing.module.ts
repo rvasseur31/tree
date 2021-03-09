00177bd821fe8b9202d5f8b19732ba2f4cba3394
Arnaud <a.lefrancois@void-technologies.com>
2021-03-09 17:23:02 +0100

    init admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './resources/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'contact',
     loadChildren: () => import('./contact/contact.module').then(module => module.ContactModule) 
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(module => module.AdminModule),
+    canLoad: [AuthGuard]
   }
 ];
 
