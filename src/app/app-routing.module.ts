e1096820cbb8dff941feece2694b5b0250bfa7c7
COSTES Romain <romain.costes@ynov.com>
2021-03-16 08:56:39 +0100

    page don

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DonComponent } from './don/don.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
