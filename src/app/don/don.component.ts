7fc9e90bf46ed9dce3109b16ba5be657a8b1c45d
COSTES Romain <romain.costes@ynov.com>
2021-03-23 12:12:35 +0100

    form

--- src/app/don/don.component.ts
+++ src/app/don/don.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm } from  '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-don',
@@ -7,13 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonComponent implements OnInit {
 
-  constructor() { }
+  myForm!: FormGroup;
+  name!: FormControl;
+  montant!: FormControl;
+  numero1!: FormControl;
+  numero2!: FormControl;
+  numero3!: FormControl;
+  numero4!: FormControl;
+  ccv!: FormControl;
+  exp_mois!: FormControl;
+  exp_annee!: FormControl;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createFormControls();
+    this.createForm();
   }
 
-  
-  onTerm() {
-  
+  createFormControls() {
+    this.name = new FormControl('', Validators.required);
+    this.numero1 = new FormControl('');
+    this.numero2 = new FormControl('');
+    this.numero3 = new FormControl('');
+    this.numero4 = new FormControl('');
+    this.ccv = new FormControl('',);
+    this.exp_mois = new FormControl('01');
+    this.exp_annee = new FormControl('01');
+    this.montant = new FormControl('', Validators.required);
+  }
+
+  createForm() {
+    this.myForm = new FormGroup({
+      name: this.name,
+      montant: this.montant,
+      numero1: this.numero1,
+      numero2: this.numero2,
+      numero3: this.numero3,
+      numero4: this.numero4,
+      ccv: this.ccv,
+      exp_mois: this.exp_mois,
+      exp_annee: this.exp_annee
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    const name = form.value['name'];
+    const montant = form.value['montant'];
+    alert("Merci pour votre don de " + montant + "€");
+}
+
+  onReset(form: NgForm) {
+    console.log("Reset !");
+    form.reset();
   }
 }
